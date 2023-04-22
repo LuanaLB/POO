ed1ab7658a9e1fc73dad7dfa26c31d9c52daafc3
Luana Batista <97992822+LuanaLB@users.noreply.github.com>
2023-04-21 21:19:38 -0300

    alterções

---
+++ Aula06/Days.java
@@ -0,0 +1,11 @@
+package Aula06;
+
+import java.time.LocalDateTime;
+
+public class Days {
+
+    public static Object daysBetween(LocalDateTime dataCheckin, LocalDateTime dataCheckout) {
+        return null;
+    }
+
+}
